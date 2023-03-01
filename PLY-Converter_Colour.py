abf3d72433477b354f05665f72f4a780a134e635
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2023-03-01 16:53:13 +0100

    First version of the biomap to PLY converter added

---
+++ PLY-Converter_Colour.py
@@ -0,0 +1,90 @@
+import pandas
+import numpy
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from coloraide import Color
+Tk().withdraw()
+filename = askopenfilename() #Opens file selection
+biomap = pandas.read_csv(filename, sep=' ', header=None, names=['X','Y','Z','Time','N.A']) #Reads the opened biomap
+
+#Header Generation
+vertices = len(biomap)-1
+dimX = biomap.iloc[0][1]
+dimY = biomap.iloc[0][0]
+edges = 2*dimX*dimY-dimX-dimY #See Grid Graphs properties, seems wrong
+faces = (dimX-1)*(dimY-1)
+faces = int(faces) #Conversion to integer is necessary for PLY files
+headertp = "ply\nformat ascii 1.0\nelement vertex ", vertices, "\nproperty float64 x\nproperty float64 y\nproperty float64 z\nproperty uchar red\nproperty uchar green\nproperty uchar blue\nelement face ", faces,"\nproperty list uchar int vertex_indices""\nend_header\n"
+header = ""
+header = header.join(map(str, headertp))
+
+#Vertices recovery
+coords = biomap.iloc[:,0:3]
+coordsfinal = coords.drop(0) #Grabs vertices coordinates without the header
+
+#MS info extraction and colouring
+intensities = biomap.iloc[:,3]
+intensities = intensities.drop(0) #Grabs intensities without the header
+intensities = intensities.fillna(0)
+imax = max(intensities)
+itstlst = []
+for i in intensities:
+    itstlst.append(int(i)) #Creates a list of intensities converted to integers
+
+########################## Colours with ColorAide
+c1 = Color("srgb", [0,1,0])
+c2 = Color("srgb",[1,0,0])
+col = Color.interpolate([c1,c2],space="srgb")
+colours = numpy.zeros(shape=(vertices,3))
+rank=0
+
+for i in itstlst:
+    hue=col(i/imax)
+    colours[rank] = ([hue['r']*255, hue['g']*255, hue['b']*255])
+    rank = rank + 1
+coloursdf = pandas.DataFrame(colours)
+coloursdf = coloursdf.astype(int)
+
+#Faces calculation
+vtx = numpy.arange(1, vertices) #Generates a numbered list corresponding to vertices
+fcs = numpy.zeros(shape=(vertices,5))
+for i in vtx:
+    if (i % dimX) != 0 and i + dimX <= vertices and ((i + dimX) % dimX) != 0 and i + dimX + 1 <= vertices:
+        #fcs[i - 1] = ([4, i-1, i, i+dimX, i+dimX-1])
+        fcs[i - 1] = ([4, i+dimX-1, i+dimX, i, i-1])
+fcsdf = pandas.DataFrame(fcs)
+fcsdf = fcsdf.astype(int)
+fcsdf = fcsdf[fcsdf[0] != 0]
+
+#Export file name recovery
+tgtname = ''
+rvstgtname = ''
+for i in reversed(filename):
+    if i != "/":
+        rvstgtname = rvstgtname + i
+    else:
+        break
+for i in reversed(rvstgtname):
+    tgtname = tgtname + i
+tgtname = tgtname.replace("biomap","ply")
+with open(tgtname,"w") as plyfile:
+    plyfile.write(header)
+counter = numpy.arange(1,(vertices+0.5)*2)
+countervtx = 0
+countercol = 0
+for i in counter: #Fills a buffer with either vertices or colours and writes it to target file
+    if i%2!=0:
+        buffertp = (coordsfinal.iloc[countervtx,0], " ", coordsfinal.iloc[countervtx,1], " ", coordsfinal.iloc[countervtx,2]," \n")
+        buffer = ""
+        buffer = buffer.join(map(str, buffertp))
+        with open(tgtname, "a") as plyfile:
+            plyfile.write(buffer)
+        countervtx = countervtx + 1
+    else:
+        buffertp = (coloursdf.iloc[countercol, 0], " ", coloursdf.iloc[countercol, 1], " ", coloursdf.iloc[countercol, 2], " \n")
+        buffer = ""
+        buffer = buffer.join(map(str, buffertp))
+        with open(tgtname, "a") as plyfile:
+            plyfile.write(buffer)
+        countercol = countercol + 1
+fcsdf.to_csv(path_or_buf=tgtname, sep=" ", header=False, index=False, mode="a") #Write faces to target file
