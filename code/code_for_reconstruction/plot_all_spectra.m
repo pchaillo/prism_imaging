ba95b76514b4af0d73981cdc717f1e108a01e6e7
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2023-05-15 12:52:49 +0200

    Added zoom functionnality to Map From Spectra

--- code/code_for_reconstruction/plot_all_spectra.m
+++ code/code_for_reconstruction/plot_all_spectra.m
@@ -1,4 +1,4 @@
-function all_peaks = plot_all_spectra(bio_dat,compute_flag,all_peaks)
+function all_peaks = plot_all_spectra(bio_dat,compute_flag,all_peaks, valmin, valmax)
 
 % compute_flag : booleen qui détermine s'il faut recalculer un spectre
 % global
@@ -8,7 +8,20 @@ if compute_flag == 0
     compute_flag = 1;
 end
 
+parsed_all_peaks = zeros(length(all_peaks), 2);
+for i = 1:length(all_peaks)
+    if all_peaks(i) >= valmin && all_peaks(i) <= valmax
+        parsed_all_peaks(i, :) = all_peaks(i, :);
+    end
+end
+
+for i = length(parsed_all_peaks):-1:1
+    if parsed_all_peaks(i) == 0
+        parsed_all_peaks(i, :) = [];
+    end
+end
+
 figure()
-plot(all_peaks(:,1),all_peaks(:,2));
+plot(parsed_all_peaks(:,1),parsed_all_peaks(:,2));
 xlabel('Mass/Charge (M/Z)')
 ylabel('Relative Intensity')
