c89d1ff4bbc2bca0602e4116992b8bb88575391d
yanisZirem <paul.chaillou@inria.fr>
2023-09-15 15:15:50 +0200

    minor improvement

--- code/code_for_recording/check_isempty.m
+++ code/code_for_recording/check_isempty.m
@@ -3,7 +3,7 @@ function check_isempty(data,file_type_text)
 % Check if the field is empty, and if it is, send a message text
 
 if isempty(data)
-    message = strcat('ATTENTION : nom du fichier [', file_type_text, '] non renseigne => indispensable pour la reconstruction');
+    message = strcat('ATTENTION : file of type [', file_type_text, '] name not filled');
     disp(message)
     f = msgbox(message);
 end
