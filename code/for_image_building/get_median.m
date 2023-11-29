3324249d1c398abde7c593eff33794f276eb30db
pchaillo <paul.chaillou@inria.fr>
2023-11-29 14:41:39 +0100

    minor cleaning

--- code/for_image_building/get_median.m
+++ code/for_image_building/get_median.m
@@ -1,16 +1,13 @@
-% détermine une médiane plus juste pour la normalisation (ne prend en compte que
-% les totIonCurrent des peaks)
-
 function median_value = get_median(bio_dat)
+
+% Will peak the good median value for normalization based on totIonCurrent values
 
 for i = 1 : length(bio_dat)
     num_tab(i) = bio_dat(i).num;
     ionisationEnergy_tab(i) = bio_dat(i).ionisationEnergy;
 end
 
-ind_peaks = find(num_tab>0);
-
-% num_reel = num_tab(ind_peaks);
+ind_peaks = find(num_tab > 0);
 
 ionisationEnergy_peaks = ionisationEnergy_tab(ind_peaks);
 
