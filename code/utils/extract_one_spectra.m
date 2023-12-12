beb782d4fa06da78d643f9270c468f147cd3ee11
pchaillo <paul.chaillou@tutanota.com>
2023-12-12 11:29:29 +0100

    preclean extract_one_spectra.m

--- code/utils/extract_one_spectra.m
+++ code/utils/extract_one_spectra.m
@@ -2,46 +2,42 @@
 
 function extract_one_spectra(pixels_scans,map,limits)
 
-carte_x = map.x;
-carte_y = map.y;
-carte_z = map.z;
 
 if isfield(carte,'time')
-    carte_time = map.time;
     time_flag = 1;
 else
     time_flag = 0;
 end
 
 loud_flag = 1;
-[ bio_ind ,bio_map ] = mzXML_on_map_norm13(pixels_scans,carte_z,limits,carte_time,time_flag);
+[ bio_ind ,pixels_mz ] = mzXML_on_map_norm13(pixels_scans,map.z,limits,map.time,time_flag);
 % [ bio_ind ,bio_map ] = data_on_map(pixels_scans,carte_z,limits,carte_time,time_flag,loud_flag); % not working ? #TODO
 
-[ carte_x,carte_y,carte_z,bio_map  ] = fix_border_2(carte_x,carte_y,carte_z,bio_map,bio_ind);
+[ map.x,map.y,map.z,pixels_mz  ] = fix_border_2(map.x,map.y,map.z,pixels_mz,bio_ind);
 
-figure() % fonction d'affichage a factoriser #TODO
-hold on
-s = surf(carte_x,carte_y,carte_z,bio_map);
- s.FaceColor = 'flat'; % set color interpolqtion
- s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
-%title(['biometric map, mass limits : ', num2str(import_limits(1)),'  ',num2str(import_limits(2))]);
-axis equal
-%grid off
-axis off
-view(2)
-hold off
+% figure() % fonction d'affichage a factoriser #TODO
+% hold on
+% s = surf(carte_x,carte_y,carte_z,bio_map);
+%  s.FaceColor = 'flat'; % set color interpolqtion
+%  s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
+% %title(['biometric map, mass limits : ', num2str(import_limits(1)),'  ',num2str(import_limits(2))]);
+% axis equal
+% %grid off
+% axis off
+% view(2)
+% hold off
+
+display_mz_map(map,pixels_mz,display = "3D")
 
 [x1,y1] = ginput(1);
 
-
-%% Solution 1 
-carte_x2 = carte_x - x1;
-min_x = min(abs(carte_x2));
+map.x2 = map.x - x1;
+min_x = min(abs(map.x2));
 
 % [p1,m1] = find( carte_x > x1 );
-[p2,m2] = find( carte_x < x1 );
+[p2,m2] = find( map.x < x1 );
 % [p3,m3] = find( carte_y > y1 );
-[p4,m4] = find( carte_y < y1 );
+[p4,m4] = find( map.y < y1 );
 
 % min_x = min(p1);
 max_x = max(p2);
