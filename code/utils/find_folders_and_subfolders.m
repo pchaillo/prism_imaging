15f6e0492155cfb93476a5a2500510461d5932c1
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2023-12-12 19:03:30 +0100

    rajout fonctionnalité de lecture de dossiers

---
+++ code/utils/find_folders_and_subfolders.m
@@ -0,0 +1,16 @@
+
+function list_folders = find_folders_and_subfolders(folder_name)
+
+folders = folder_scan(folder_name);
+size_folders = size(folders);
+disp(size_folders);
+list_folders = folders;
+if size_folders(2) > 0
+    for i = 3 : size_folders(2)
+        sub_folders = folder_scan_recursive(folder_name + "/" + folders(i));
+        list_folders = list_folders + sub_folders;
+        disp(list_folders);
+    end
+end
+disp(list_folders);
+end
