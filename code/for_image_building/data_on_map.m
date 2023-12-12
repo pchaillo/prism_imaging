537b737696e157212ffdbcd722b32046376943aa
pchaillo <paul.chaillou@tutanota.com>
2023-12-12 12:09:17 +0100

    clean variable name in data_on_map function

--- code/for_image_building/data_on_map.m
+++ code/for_image_building/data_on_map.m
@@ -3,7 +3,7 @@
 % Pour permettre l'affichage des informations biométriques sur la carte
 % topographique
 
-% indépendant du pattern, se base sur carte_time
+% indépendant du pattern, se base sur map_time
 
 % avec une médiane plus juste pour la normalisation (ne prend en compte que
 % les totIonCurrent des peaks) + totIonCurrent additionné pour avoir une
@@ -11,26 +11,28 @@
 
 % ne crée pas de valeur pour les points ajoutés
 
-function [ bio_ind, bio_num, bio_map, deiso_tab] = data_on_map(bio_dat,map,limits,carte_time,time_flag,loud_flag)
+function [ pixels_ind, scans_ind, pixels_mz, fusion_tab] = data_on_map(pixels_scans,map,limits,map_time,time_flag,loud_flag)
 % anciennement mzXML_on_map17.m
+% scans_ind (bio_num) => indices in the full mzXML file
+% pixels_ind (bio_ind) => indices of the relative pixel in the image
 
 si = size(map);
 
-if isempty(bio_dat(1).ionisationEnergy) % pour pouvoir utiliser les vieux mat files
-    for i = 1:length(bio_dat)
-        bio_dat(i).ionisationEnergy = bio_dat(i).totIonCurrent;
+if isempty(pixels_scans(1).ionisationEnergy) % pour pouvoir utiliser les vieux mat files
+    for i = 1:length(pixels_scans)
+        pixels_scans(i).ionisationEnergy = pixels_scans(i).totIonCurrent;
     end
 end
 
 %Extraction des données utiles
 id = 0;
-for i = 1:length(bio_dat)
-   % if bio_dat(i).num ~= 2 %to delete useless empty point
+for i = 1:length(pixels_scans)
+   % if pixels_scans(i).num ~= 2 %to delete useless empty point
         id = id +1 ;
-        ionisationEnergy(id) = bio_dat(i).ionisationEnergy ;
+        ionisationEnergy(id) = pixels_scans(i).ionisationEnergy ;
         if loud_flag == 0
-            if bio_dat(i).num > 0
-                value(id) = peak_selection( limits,bio_dat(i) )/ionisationEnergy(id);
+            if pixels_scans(i).num > 0
+                value(id) = peak_selection( limits,pixels_scans(i) )/ionisationEnergy(id);
             else
                 value(id) = 0;
             end
@@ -39,34 +41,34 @@ for i = 1:length(bio_dat)
                 value(id) = 0;
             else
                 
-                value(id) = peak_selection( limits,bio_dat(i) )/ionisationEnergy(id);
+                value(id) = peak_selection( limits,pixels_scans(i) )/ionisationEnergy(id);
             end
         end
   %  end
 end
 
 % med = median(totIonCurrent,'all'); %% be carefull change to median
-med = get_median(bio_dat);
+med = get_median(pixels_scans);
 
 % si le temps est enregistré, on l'utilise pour replacer les informations
 % sur la carte
 if time_flag == 1
     
-    carte_ind = time_to_indice(carte_time);
+    carte_ind = time_to_indice(map_time);
     
     d_ind = 0    ;
     
     for i =  1  :  si(1)
         for j = 1 : si(2)
             d_ind = d_ind + 1;
-            bio_map(i,j)= value(carte_ind(i,j))*med;
-            %             bio_num(i,j) = bio_dat.num(carte_ind(i,j));
-            bio_num(i,j) = bio_dat(carte_ind(i,j)).num;
-            deiso_tab(d_ind) = {bio_dat(carte_ind(i,j)).deisotoped};
+            pixels_mz(i,j)= value(carte_ind(i,j))*med;
+            %             scans_ind(i,j) = pixels_scans.num(carte_ind(i,j));
+            scans_ind(i,j) = pixels_scans(carte_ind(i,j)).num;
+            pixels_mz(d_ind) = {pixels_scans(carte_ind(i,j)).deisotoped};
         end
     end
     
-    bio_ind = carte_ind ;
+    pixels_ind = carte_ind ;
     
     % sinon on suit le pattern classique
 else
@@ -79,15 +81,15 @@ else
             for j = 1 : si(2)
                 u = u +1;
                 k = k+1;
-                bio_ind(v,u) = k;
-                bio_num(v,u) = bio_dat(k).num;
+                pixels_ind(v,u) = k;
+                scans_ind(v,u) = pixels_scans(k).num;
                 d_ind = d_ind + 1;
-                deiso_tab(d_ind) = {bio_dat(k).deisotoped};
+                fusion_tab(d_ind) = {pixels_scans(k).deisotoped};
                 k
                 if ionisationEnergy(k) ~= 0
-                    bio_map(v,u)= value(k)*med;
+                    pixels_mz(v,u)= value(k)*med;
                 else
-                    bio_map(v,u) = 0;
+                    pixels_mz(v,u) = 0;
                 end
             end
         else
@@ -95,14 +97,14 @@ else
             for j = si(2) : -1  : 1
                 u = u - 1;
                 k = k+1;
-                bio_ind(v,u) = k;
-                bio_num(v,u) = bio_dat(k).num;
+                pixels_ind(v,u) = k;
+                scans_ind(v,u) = pixels_scans(k).num;
                 d_ind = d_ind + 1;
-                deiso_tab(d_ind) = {bio_dat(k).deisotoped};
+                fusion_tab(d_ind) = {pixels_scans(k).deisotoped};
                 if ionisationEnergy(k) ~= 0
-                    bio_map(v,u)= value(k)*med;
+                    pixels_mz(v,u)= value(k)*med;
                 else
-                    bio_map(v,u) = 0;
+                    pixels_mz(v,u) = 0;
                 end
             end
         end
