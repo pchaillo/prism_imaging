5a33ea48f75525fb1540fda8b4bd0cf0f312d8e0
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2023-12-12 11:00:35 +0100

    Removed a deprecated folder

--- old data/prism_imaging-new_laser/code/code_for_mapping/trigger_spectro_time.m
+++
@@ -1,7 +0,0 @@
-function time_ref = trigger_spectro_time(a)
-
-writeDigitalPin(a,'A5',0) 
-pause(0.1)
-writeDigitalPin(a,'A5',1)
-
-time_ref = tic;
--- old data/prism_imaging-new_laser/code/code_for_preprocessing/clean_time.m
+++
@@ -1,9 +0,0 @@
-function file2 = clean_time(file)
-
-file2 = file;
-
-for i = 1 : length(file)
-    time_raw = file(i).retentionTime;
-    time = raw_to_time(time_raw);
-    file2(i).retentionTime= time;
-end
