88f5df457d28bf9ac8d77182f630efd335f31ea1
pchaillo <paul.chaillou@tutanota.com>
2023-12-12 17:09:22 +0100

    fix minor variable name error

--- code/utils/for_casting/time_to_indice.m
+++ code/utils/for_casting/time_to_indice.m
@@ -1,6 +1,10 @@
 function map_ind = time_to_indice(map_time)
 
-time_list = time_to_list(map_time)
+% this function replace the time in the map_time variable by indices (with the same shape),
+% deduct by the time (croissant)
+
+
+time_list = time_to_list(map_time);
 
 corrected_time_list = unique(time_list); % Indispensable ? #TODO
 
