d7bfee0c02a22126891958a8ad03ecf6b3285310
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2023-12-13 10:16:18 +0100

    fonction find_folders_and_subfolders terminée

--- code/utils/find_folders_and_subfolders.m
+++ code/utils/find_folders_and_subfolders.m
@@ -1,15 +1,15 @@
 
 function list_folders = find_folders_and_subfolders(folder_name)
 
-folders = folder_scan(folder_name);
+[folders,files] = sort_folders_and_files(folder_name);
 size_folders = size(folders);
-disp(size_folders);
-list_folders = folders;
-if size_folders(2) > 0
-    for i = 3 : size_folders(2)
-        sub_folders = folder_scan_recursive(folder_name + "/" + folders(i));
-        list_folders = list_folders + sub_folders;
-        disp(list_folders);
+if size_folders(1) > 0
+    list_folders = [folders];
+    for i = 1 : size_folders(1)
+        sub_folders = find_folders_and_subfolders(folder_name + "/" + folders(i));
+        list_folders = [list_folders;sub_folders];
     end
+else 
+    list_folders = [];
 end
 end
