3e5792ab26bc184ca7b5894ceb37c8242e619f15
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2023-12-13 10:39:16 +0100

    fix bug nom des dossiers

--- code/utils/sort_folders_and_files.m
+++ code/utils/sort_folders_and_files.m
@@ -6,8 +6,8 @@ function [folders,files] = sort_folders_and_files(folder_name)
 for i = 3 : si(2)
     sub = folder_scan(folder_name+"/"+files_and_folders(i));
     if sub(1) == "." && sub(2) == ".."  
-        folders = [folders;files_and_folders(i)];
+        folders = [folders;folder_name+"/"+files_and_folders(i)];
     else
-        files = [files;files_and_folders(i)];
+        files = [files;folder_name+"/"+files_and_folders(i)];
     end
 end
