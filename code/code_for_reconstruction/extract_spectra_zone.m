9b193008baeae5c0b1edec00560629d0366cd90f
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2023-12-15 15:55:39 +0100

    fix affichage extract spectra zone

--- code/code_for_reconstruction/extract_spectra_zone.m
+++ code/code_for_reconstruction/extract_spectra_zone.m
@@ -1,46 +1,28 @@
-function extract_spectra_zone3(bio_dat,carte,limits)
-
-carte_x = carte.x;
-carte_y = carte.y;
-carte_z = carte.z;
-seuil = -1;
+function extract_spectra_zone(bio_dat,carte,limits)
 
 if isfield(carte,'time')
-    carte_time = carte.time;
     time_flag = 1;
 else
     time_flag = 0;
 end
 
-loud_flag = 0;
-[ bio_ind ,bio_map ] = mzXML_on_map_norm17(bio_dat,carte_z,limits,carte_time,time_flag,loud_flag); 
+[ bio_ind ,bio_map ] = mzXML_on_map_norm13(bio_dat,carte.z,limits,carte.time,time_flag); 
 
-[ carte_x,carte_y,carte_z,bio_map ] = fix_border_2(carte_x,carte_y,carte_z,bio_map,bio_ind);
+[ carte.x,carte.y,carte.z,bio_map ] = fix_border_2(carte.x,carte.y,carte.z,bio_map,bio_ind);
 
-figure()
-hold on
-s = surf(carte_x,carte_y,carte_z,bio_map);
-%s.FaceAlpha=0.9; % niveau de tranparence
-s.FaceColor = 'flat'; % set color interpolqtion
-s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
-%title(['biometric map, mass limits : ', num2str(import_limits(1)),'  ',num2str(import_limits(2))]);
-axis equal
-%grid off
-axis off
-view(2)
-hold off
+display_mz_map(carte,bio_map)
 
 [x1,y1] = ginput(1);
 [x2,y2] = ginput(1);
 
-[p2,m2] = find( carte_x < x1 );
-[p4,m4] = find( carte_y < y1 );
+[p2,m2] = find( carte.x < x1 );
+[p4,m4] = find( carte.y < y1 );
 
 ind_x1 = max(p2);
 ind_y1 = max(m4); 
 
-[p1,m1] = find( carte_x < x2 );
-[p3,m3] = find( carte_y < y2 );
+[p1,m1] = find( carte.x < x2 );
+[p3,m3] = find( carte.y < y2 );
 
 ind_x2 = max(p1);
 ind_y2 = max(m3); 
@@ -65,7 +47,7 @@ max_int_value = max(max(bio_map));
  [I,J] = find(bio_map == max_int_value) ;
 
 id = 0;
-size_map = size(carte_x);
+size_map = size(carte.x);
 map_color = bio_map;
 for x_ind = min_x : max_x % récupère les indices
     for y_ind = min_y : max_y
@@ -103,7 +85,7 @@ times = times';
 
 %% Fin de la fonction
 figure()
-s = surf(carte_x,carte_y,carte_z,map_color);
+s = surf(carte.x,carte.y,carte.z,map_color);
 s.FaceAlpha=0.9; % niveau de tranparence
 s.FaceColor = 'flat'; % set color interpolqtion
 s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
@@ -111,7 +93,6 @@ title('Zone of the mass spectra');
 axis equal
 %grid off
 axis off
-
 si_p = size(peaks);
 
 win = 0.1;
@@ -137,5 +118,4 @@ ylabel('Relative Intensity')
 title('Sum of all the spectra of the zone');
 
 csv_spectra_recorder(peak_tab_norm_sum,"Test_sauv_somme.csv")
-
 
--- code/utils/extract_one_spectra.m
+++ code/utils/extract_one_spectra.m
@@ -9,26 +9,12 @@ else
     time_flag = 0;
 end
 
-loud_flag = 1;
 [ bio_ind ,pixels_mz ] = mzXML_on_map_norm13(pixels_scans,map.z,limits,map.time,time_flag);
-% [ bio_ind ,bio_map ] = data_on_map(pixels_scans,carte_z,limits,carte_time,time_flag,loud_flag); % not working ? #TODO
 
 [ map.x,map.y,map.z,pixels_mz  ] = fix_border_2(map.x,map.y,map.z,pixels_mz,bio_ind);
 
-% figure() % fonction d'affichage a factoriser #TODO
-% hold on
-% s = surf(carte_x,carte_y,carte_z,bio_map);
-%  s.FaceColor = 'flat'; % set color interpolqtion
-%  s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
-% %title(['biometric map, mass limits : ', num2str(import_limits(1)),'  ',num2str(import_limits(2))]);
-% axis equal
-% %grid off
-% axis off
-% view(2)
-% hold off
-
 display_mz_map(map,pixels_mz)
-
+%{
 [x1,y1] = ginput(1);
 
 map.x2 = map.x - x1;
@@ -57,3 +43,4 @@ figure();
 plot(peaks(:,1),peaks(:,2));
 xlabel('Mass/Charge (M/Z)');
 ylabel('Relative Intensity');
+%}
