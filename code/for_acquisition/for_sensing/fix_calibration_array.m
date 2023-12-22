f771c94738c03641a599882048780e0eef0c66a6
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2023-12-22 15:24:57 +0100

    Translated some comments and variable names to English.

--- code/for_acquisition/for_sensing/fix_calibration_array.m
+++ code/for_acquisition/for_sensing/fix_calibration_array.m
@@ -1,27 +1,27 @@
 function corrected_calibration_array = fix_calibration_array(calibration_array)
 
-% supprime les valeurs fausses
-% nouvelle version pour ILD1320-25
+% Removes erroneous values
+% New version for the ILD1320-25 sensor
 
 supp = 1 ;
-arret = 0 ;
-f =  length(calibration_array)-1 ;
+stop = 0 ;
+f =  length(calibration_array)-1 ; % Why "f"?
 c_sup = 0;
 while supp ~= 0
     supp = 0;
     f = length(calibration_array)-1;
     
-    while arret == 0
+    while stop == 0
         
         if f == 2
-            arret = 1;
+            stop = 1;
         end
         
         if f >= length(calibration_array)
             f = length(calibration_array)-1;
         end
         
-        if abs( calibration_array(2,f+1) ) < abs(calibration_array(2,f) )
+        if abs(calibration_array(2,f+1)) < abs(calibration_array(2,f) )
             calibration_array(:,f+1) = [];
             supp = supp + 1;
         else
@@ -38,16 +38,16 @@ t_sup = sum(tab_sup)
 to_supp = -1;
 
 if t_sup > 100
-    disp('tableau faussé');
+    disp('Warning: Biased table');
 end
 
-for i = 1 :  length(calibration_array) % pour corriger les valeurs concordantes mais tout de meme fausses
+for i = 1 :  length(calibration_array) % Corrects sensible values that are wrong nonetheless
     if calibration_array(2,i) <0.8
         to_supp = [to_supp i];
     end
 end
 
-to_supp(1) = []; % on retire le -1 mis initialement
+to_supp(1) = []; % Removes the initial -1 offset
 
 calibration_array(:,to_supp) = [];
 
