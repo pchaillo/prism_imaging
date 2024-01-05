fdfc49e293fac7e4abb41c1ab44accb7b8753beb
Paul Chaillou <paul.chaillou@tutanota.com>
2024-01-05 16:43:59 +0100

    fix window name to bining_step

--- code/for_processing/bining_fixed_size.m
+++ code/for_processing/bining_fixed_size.m
@@ -1,13 +1,13 @@
 % Réalise le groupement des données
 % remet le tableau dans une forme finie définie par band, la bande de
-% masses qui nous interesse et step, la largeur de la fenetre de bining
+% masses qui nous interesse et bining_step, la largeur de la fenetre de bining
 
 % forme finie => comparable entre elles
 % FORME NORMALISé
 
 % Copie de bining_2 (anciennment bining_norm)
 
-function bined_peak_array = bining_fixed_size(peak_array,step,band)
+function bined_peak_array = bining_fixed_size(peak_array,bining_step,band)
 
 band_begin = band(1);
 band_end = band(2);
@@ -20,10 +20,10 @@ bined_peak_array = zeros(1,2); % for c
 
 ind_p = 0;
 
-for mz = band_begin : step : band_end - step
+for mz = band_begin : bining_step : band_end - bining_step
     
-    all_peaks_over_mass_array = find ( peak_array(:,1) > mz );%&& peak_array(:,1) < mz +step);
-    all_peaks_under_max_mass_array = find( peak_array(:,1) < mz + step);
+    all_peaks_over_mass_array = find ( peak_array(:,1) > mz );%&& peak_array(:,1) < mz +bining_step);
+    all_peaks_under_max_mass_array = find( peak_array(:,1) < mz + bining_step);
     max_ind = max(all_peaks_under_max_mass_array);
     min_ind = min(all_peaks_over_mass_array);
     
@@ -32,7 +32,7 @@ for mz = band_begin : step : band_end - step
     if min_ind < max_ind
         bined_peak_array(ind_in_bined_array,2) = sum(peak_array(min_ind:max_ind,2));
         bined_peak_array(ind_in_bined_array,1) = mz;
-    elseif min_ind == max_ind % if there is only one m/z that fit to the step in peak_aray
+    elseif min_ind == max_ind % if there is only one m/z that fit to the bining_step in peak_aray
         bined_peak_array(ind_in_bined_array,2) = peak_array(min_ind,2);
         bined_peak_array(ind_in_bined_array,1) = mz;
     else
