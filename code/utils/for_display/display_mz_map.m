c4797fbec0b744f90e55d00a0cd6128eed1fed07
Paul Chaillou <paul.chaillou@tutanota.com>
2024-01-05 15:19:53 +0100

    cleaning ongoing, fix minor bugs

--- code/utils/for_display/display_mz_map.m
+++ code/utils/for_display/display_mz_map.m
@@ -2,15 +2,15 @@ function display_mz_map(map,pixels_mz,title_str)
 % to see image as 2D from front display = "2D". Otherwise pick "3D"
 
 figure() % fonction d'affichage a factoriser #TODO
-hold on
+% hold on
 s = surf(map.x,map.y,map.z,pixels_mz);
 s.FaceColor = 'flat'; % set color interpolqtion
 s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
 title(title_str);
 axis equal
 %grid off
-axis off
+% axis off
 % if display == "2D"
 %     view(2)
 % end
-hold off
+% hold off
