325dbc94b66c94cb6d107b291bd4964295b44c9e
unknown <paul.chaillou@tutanota.com>
2024-02-22 12:24:37 +0100

    debug ongoing

--- code/for_image_building/for_scan_selection/TimeBased/add_multiple_neighbourgh_scan.m
+++ code/for_image_building/for_scan_selection/TimeBased/add_multiple_neighbourgh_scan.m
@@ -5,6 +5,6 @@ function all_scans = add_multiple_neighbourgh_scan(all_scans,selected_indices,ne
 for i=1 : length(selected_indices)-1
     for n = 1 : neighbourgh_nb
         [all_scans(selected_indices(i)), all_scans(selected_indices(i)+n)] = fusion_part_A_and_B_output( all_scans(selected_indices(i)), all_scans(selected_indices(i)+n) );
-        [all_scans(selected_indices(i)), all_scans(selected_indices(i)-n)]  = fusion_part_A_and_B_output( all_scans(selected_indices(i)), all_scans(selected_indices(i)-n));
+        [all_scans(selected_indices(i)), all_scans(selected_indices(i)-n)] = fusion_part_A_and_B_output( all_scans(selected_indices(i)), all_scans(selected_indices(i)-n));
     end
 end
