cf3a5a2ce43cd1bc03d82cf51c5e7ec795f9086a
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2024-02-22 14:17:50 +0100

    Fixed an edge case with Time Based peak selection where the program would try fetching scans outside of their index range

--- code/for_image_building/for_scan_selection/TimeBased/add_multiple_neighbourgh_scan.m
+++ code/for_image_building/for_scan_selection/TimeBased/add_multiple_neighbourgh_scan.m
@@ -1,10 +1,12 @@
-function all_scans = add_multiple_neighbourgh_scan(all_scans,selected_indices,neighbourgh_nb)
+function all_scans = add_multiple_neighbourgh_scan(all_scans, selected_indices, neighbourgh_nb)
 
-% Function to add next scan fusion (to be sure to not miss any useful data
+% Function to add next scan fusion (To ensure all useful data is kept)
 
 for i=1 : length(selected_indices)-1
     for n = 1 : neighbourgh_nb
+        if selected_indices - n > 1
         [all_scans(selected_indices(i)), all_scans(selected_indices(i)+n)] = fusion_part_A_and_B_output( all_scans(selected_indices(i)), all_scans(selected_indices(i)+n) );
         [all_scans(selected_indices(i)), all_scans(selected_indices(i)-n)]  = fusion_part_A_and_B_output( all_scans(selected_indices(i)), all_scans(selected_indices(i)-n));
+        end 
     end
 end
