3288ceb07540c441167e6e54b74c557133c608bd
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2024-02-23 10:55:20 +0100

    Slight tweak of e_stop.m

--- code/hardware/e_stop.m
+++ code/hardware/e_stop.m
@@ -5,11 +5,11 @@ function e_stop(source, robot, arduino, sensor, imaging_mode, app)
 
 robot.class.disconnect(app);
 
+clear(arduino)
+clear(sensor)
+
 if imaging_mode ~= app.TopographyOnlyButton
     source.class.STOP_continuous_trigerring(app);
 end
-
-clear(arduino)
-clear(sensor)
 
 end
