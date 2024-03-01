db5c90955e85ee963e150e44e3eaf1107256d64f
unknown <paul.chaillou@tutanota.com>
2024-03-01 14:47:57 +0100

    minor fix on chromatogram plotting

--- code/utils/for_display/plot_chromatogram.m
+++ code/utils/for_display/plot_chromatogram.m
@@ -7,4 +7,6 @@ TIC_list = extract_TIC(all_scans);
 Scan_time = extract_time(all_scans);
 
 plot(Scan_time,TIC_list)
-
+xlabel('Time (s)')
+ylabel('TIC intensity')
+title('Scan selection on chromatogram')
