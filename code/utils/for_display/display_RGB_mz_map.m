107e9541edeb32a578113f2ed8ab38119621e585
Paul Chaillou <paul.chaillou@tutanota.com>
2024-04-25 16:18:15 +0200

    button added, need to be tested

--- code/utils/for_display/display_RGB_mz_map.m
+++ code/utils/for_display/display_RGB_mz_map.m
@@ -1,4 +1,4 @@
-function display_RGB_mz_map(map,pixels_mz_R,pixels_mz_G,pixels_mz_B,mz1,mz2,mz3,title_str)
+function display_RGB_mz_map(map,pixels_mz_R,pixels_mz_G,pixels_mz_B,limits_R,limits_G,limits_B,title_str)
 
 % to see image as 2D from front display = "2D". Otherwise pick "3D"
 
@@ -11,5 +11,5 @@ s = surf(map.x,map.y,map.z,rgb_map);
 s.FaceAlpha=0.9; % niveau de tranparence
 s.FaceColor = 'flat'; % set color interpolqtion
 s.EdgeColor = 'none'; %'none' disable lines, you can also choose the color : 'white', etc.
-title(['RGB biometric map, mass limits : ', num2str(mz1),'  ',num2str(mz2),' ',num2str(mz3), title_str]);
+title(['RGB biometric map, mass limits : R =', num2str(limits_R),' G = ',num2str(limits_G),' B = ',num2str(limits_B), title_str]);
 axis equal
