7d3084d6a812541915fed8a3ee2d9f9d8ab93616
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2024-06-12 15:19:07 +0200

    Fixed mzml conversion. Testing is required to make sure that the path syntax works. Something feels really off about the version that was left here, I am positive that my previous latest version did not look like that.

--- code/code_python/raw_conversion/raw_conversion.py
+++ code/code_python/raw_conversion/raw_conversion.py
@@ -3,20 +3,20 @@ import subprocess
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 
-# TODO: Add a dictionnary of all output formats and pass that as an argument instead of hardcoding mzML as an output
+# TODO: Add support for the dictionnary in MatLab
 
 extensions_dict = {'mzML':'--mzML', 'mzXML':'--mzXML', 'mgf':'--mgf', 'txt':'--txt', 'mz5':'--mz5'}
 
-def convert_raw_to_mzxml(raw_files_path, txt_file, extension = "--mzML"):
-    command = ["msconvert", raw_files_path, "-o", 'files/conversion/', "extension=",extension]
+def convert_raw_to_mzxml(raw_file_path, txt_file, extension = "--mzML"):
+    command = ["msconvert ", raw_file_path, "-f ", txt_file, "-o ", "files/conversion/ ", "--mzML"]
     subprocess.run(command, check=True)
     
-print("Loaded text file : ")
+print("Loaded text file: ")
 print(txt_file)
 
-Tk().withdraw()
-filename = askopenfilename()
+print("Loaded .RAW file: ")
+print(raw_file)
 
-convert_raw_to_mzxml(raw_files_path=filename, txt_file = txt_file)
+convert_raw_to_mzxml(raw_file_path=raw_file, txt_file = txt_file)
 
 print("mzML file saved in files/raw files/")
