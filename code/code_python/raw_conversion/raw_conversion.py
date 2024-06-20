ed38ee10a7f43c9af2850777984c99da543770bb
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2024-06-20 15:23:22 +0200

    Updated the .gitignore file to inlcude mzML files. Commented some txt file generation-related code

--- code/code_python/raw_conversion/raw_conversion.py
+++ code/code_python/raw_conversion/raw_conversion.py
@@ -8,7 +8,8 @@ from tkinter import Tk
 extensions_dict = {'mzML':'--mzML', 'mzXML':'--mzXML', 'mgf':'--mgf', 'txt':'--txt', 'mz5':'--mz5'}
 
 def convert_raw_to_mzxml(raw_file_path, txt_file, output_loc, extension = "--mzML"):
-    command = ["msconvert ", '"', raw_file_path, '"', "-f ", '"', txt_file, '"', "-o ", output_loc, "--mzML"]
+    command = ["msconvert ", '"', raw_file_path, '" ', "-f ", '"', txt_file, '" ', "-o ", output_loc, " --mzML"]
+    command = ''.join(command)
     subprocess.run(command, check=True)
     
 print("Loaded text file: ")
@@ -16,6 +17,8 @@ print(txt_file)
 
 print("Loaded .RAW file: ")
 print(raw_file)
+
+print(output_loc)
 
 convert_raw_to_mzxml(raw_file_path=raw_file, txt_file=txt_file, output_loc=output_loc, extension="--mzML")
 
