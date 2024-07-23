2c52d6dc62150442f3da2999df975fd7e9039f02
unknown <adel.guot.etu@univ-lille.fr>
2024-07-23 10:53:13 +0200

    Updated the update_hardware function to current standards. Some tweaking of the mlapp file is likely required

--- update_hardware.m
+++ update_hardware.m
@@ -6,31 +6,14 @@
 % dropdown menus for the various software methods and hardware classes.
 % paths = ['./code/hardware/laser/*.m', '/code/hardware/robot/*.m', './code/hardware/sensor/*.m', './code/method/scan_selection/*.m'];
 
-addpath(genpath('3d_export/'))
 addpath(genpath('code'))
 addpath(genpath('files'))
 
-scan_methods_tab = folder_scan_without_extension('./code/method/scan_selection/*.m');
-scan_methods = [];
-for i = scan_methods_tab
+mass_spectrometer_tab = folder_scan_without_extension('./code/hardware/mass_spectrometer/*.m');
+ms_types = [];
+for i = mass_spectrometer_tab
    if i ~= "_template_(name_MUST_be_changed)"
-     scan_methods=[scan_methods;i];
-   end
-end
-
-laser_types_tab = folder_scan_without_extension('./code/hardware/laser/*.m');
-laser_types = [];
-for i = laser_types_tab
-   if i ~= "_template_(name_MUST_be_changed)"
-     laser_types=[laser_types;i];
-   end
-end
-
-sensor_types_tab = folder_scan_without_extension('./code/hardware/sensor/*.m');
-sensor_types = [];
-for i = sensor_types_tab
-   if i ~= "_template_(name_MUST_be_changed)"
-     sensor_types=[sensor_types;i];
+     ms_types=[ms_types;i];
    end
 end
 
@@ -42,25 +25,54 @@ for i = robot_types_tab
    end
 end
 
-scan_methods_names = fopen(strcat(pwd, "/code/method/scan_methods.txt"), 'W');
-laser_types_names = fopen(strcat(pwd, "/code/hardware/laser_types.txt"), 'W');
-sensor_types_names = fopen(strcat(pwd, "/code/hardware/sensor_types.txt"), 'W');
+scan_methods_tab = folder_scan_without_extension('./code/method/scan_selection/*.m');
+scan_methods = [];
+for i = scan_methods_tab
+   if i ~= "_template_(name_MUST_be_changed)"
+     scan_methods=[scan_methods;i];
+   end
+end
+
+sensor_types_tab = folder_scan_without_extension('./code/hardware/sensor/*.m');
+sensor_types = [];
+for i = sensor_types_tab
+   if not(ismember(i, ["_template_(name_MUST_be_changed)", "AnalogicSensorBase", "NumericSensorBase"]))
+     sensor_types=[sensor_types;i];
+   end
+end
+
+source_types_tab = folder_scan_without_extension('./code/hardware/source/*.m');
+source_types = []; %Formerly "laser_types"
+for i = source_types_tab
+   if i ~= "_template_(name_MUST_be_changed)"
+     source_types=[source_types;i];
+   end
+end
+
+mass_spectrometer_types_names = fopen(strcat(pwd, "/code/hardware/mass_spectrometer_types.txt"), 'W');
 robot_types_names = fopen(strcat(pwd, "/code/hardware/robot_types.txt"), 'W');
+scan_methods_names = fopen(strcat(pwd, "/code/method/scan_methods.txt"), 'W');
+sensor_types_names = fopen(strcat(pwd, "/code/hardware/sensor_types.txt"), 'W');
+source_types_names = fopen(strcat(pwd, "/code/hardware/source_types.txt"), 'W');
+
+for i = ms_types
+    fprintf(mass_spectrometer_types_names, '%s\n', i);
+end
+
+for i = robot_types
+    fprintf(robot_types_names, '%s\n', i);
+end
 
 for i = scan_methods
     fprintf(scan_methods_names, '%s\n', i);
-end
-
-for i = laser_types
-    fprintf(laser_types_names, '%s\n', i);
 end
 
 for i = sensor_types
     fprintf(sensor_types_names, '%s\n', i);
 end
 
-for i = robot_types
-    fprintf(robot_types_names, '%s\n', i);
+for i = source_types
+    fprintf(source_types_names, '%s\n', i);
 end
 
 fclose('all');
