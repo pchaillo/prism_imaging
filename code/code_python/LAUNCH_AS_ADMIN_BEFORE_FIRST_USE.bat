8c4b009c3c57c957d79cebf1036d661287902862
unknown <adel.guot.etu@univ-lille.fr>
2024-07-24 14:33:34 +0200

    add explanations on .bat file for automaic python libraries dowmload

--- code/code_python/LAUNCH_AS_ADMIN_BEFORE_FIRST_USE.bat
+++ code/code_python/LAUNCH_AS_ADMIN_BEFORE_FIRST_USE.bat
@@ -27,6 +27,7 @@ py -3.8 -m pip install pandas
 py -3.8 -m pip install Pillow
 py -3.8 -m pip install scipy
 py -3.8 -m pip install Tk
+echo Moving some tkinter folders around to accomodate MatLab looking for them in the wrong space.
 powershell -command "Xcopy C:\Python38\tcl\tcl8.6 C:\Python38\lib\tcl8.6 /v /i /e"
 powershell -command "Xcopy C:\Python38\tcl\tk8.6 C:\Python38\lib\tk8.6 /v /i /e"
 goto END
@@ -43,6 +44,7 @@ py -3.11 -m pip install pandas
 py -3.11 -m pip install Pillow
 py -3.11 -m pip install scipy
 py -3.11 -m pip install Tk
+echo Moving some tkinter folders around to accomodate MatLab looking for them in the wrong space.
 powershell -command "Xcopy C:\Python311\tcl\tcl8.6 C:\Python311\lib\tcl8.6 /v /i /e"
 powershell -command "Xcopy C:\Python311\tcl\tk8.6 C:\Python311\lib\tk8.6 /v /i /e"
 goto END
