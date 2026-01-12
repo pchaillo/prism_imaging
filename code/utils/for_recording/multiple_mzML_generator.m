5edbcf6acc3a4d01d09c623fb43c048898d2e89e
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2026-01-12 13:26:37 +0100

    Fixed the multiple mzML file generation. I should note that it can be extremely slow when dealing with high resolution images. Maybe it could be worth switching to multicore?

--- code/utils/for_recording/multiple_mzML_generator.m
+++ code/utils/for_recording/multiple_mzML_generator.m
@@ -1,7 +1,8 @@
-function  multiple_mzML_generator(app, raw_path, folder_txt, pixels_scans, output_file_loc,name)
+function  multiple_mzML_generator(app, raw_path, folder_txt, pixels_scans, output_file_loc, name)
 
 name_folder = strcat(name,"\");
 txt_folder_path = path_editor(folder_txt,name_folder);
+disp(txt_folder_path)
 mkdir(txt_folder_path)
 
 name_folder = strcat(name,"\");
@@ -10,7 +11,7 @@ mzML_folder_path = strcat(output_file_loc, name_folder);
 mkdir(mzML_folder_path)
 
 l = length(pixels_scans);
-for i = 1 : l % Working, but not adapted to the rest of the pipeline, avec pixels_scans en argument
+for i = 1 : l % Working, but not adapted to the rest of the pipeline, with pixels_scans en argument
     num_list = pixels_scans(i).deisotoped;
     txt_path_pixel = strcat(txt_folder_path, name , "_" , string(i),".txt");
     export_to_txt(txt_path_pixel,num_list);
