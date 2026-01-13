6918a125528da1a1aabac100dd1b7ea92d2cd9a7
AdelGuiot <adel.guiot.etu@univ-lille.fr>
2026-01-13 13:27:36 +0100

    Fixed a broken path in the multiple mzML generation. The progress bar has been properly implemented and tested.

--- code/utils/for_recording/multiple_mzML_generator.m
+++ code/utils/for_recording/multiple_mzML_generator.m
@@ -7,23 +7,15 @@ disp(txt_folder_path)
 mkdir(txt_folder_path)
 
 % mzML_folder_path = path_editor(output_file_loc,name_folder);
-mzML_folder_path = strcat(output_file_loc, "files\", name_folder);
+mzML_folder_path = strcat(output_file_loc, name_folder);
 mkdir(mzML_folder_path)
 
 l = length(pixels_scans);
 
 if l > 100
-    parfor i = 1 : l
-        num_list = pixels_scans(i).deisotoped;
-        txt_path_pixel = strcat(txt_folder_path, name , "_" , string(i),".txt");
-        export_to_txt(txt_path_pixel,num_list);
-        mzML_pixel_name = strcat( name , "_" , string(i));
-        raw_conversion(app, raw_path, txt_path_pixel, mzML_folder_path, mzML_pixel_name, false)
-    end
-else
     % Initialize the progress bar
     queue = createParallelProgressBar(l);
-    for i = 1 : l % Working, but not adapted to the rest of the pipeline, with pixels_scans en argument
+    parfor i = 1 : l
         num_list = pixels_scans(i).deisotoped;
         txt_path_pixel = strcat(txt_folder_path, name , "_" , string(i),".txt");
         export_to_txt(txt_path_pixel,num_list);
@@ -31,7 +23,18 @@ else
         raw_conversion(app, raw_path, txt_path_pixel, mzML_folder_path, mzML_pixel_name, false)
         send(queue, i)
     end
+else
+    
+    for i = 1 : l % Working, but not adapted to the rest of the pipeline, with pixels_scans en argument
+        num_list = pixels_scans(i).deisotoped;
+        txt_path_pixel = strcat(txt_folder_path, name , "_" , string(i),".txt");
+        export_to_txt(txt_path_pixel,num_list);
+        mzML_pixel_name = strcat( name , "_" , string(i));
+        raw_conversion(app, raw_path, txt_path_pixel, mzML_folder_path, mzML_pixel_name, false)
+    end
 end
+
+update_log(app, "Files exported in the project's mzML folder.")
 
 
 
